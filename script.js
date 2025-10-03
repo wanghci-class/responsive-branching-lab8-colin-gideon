4fb0cfe4d9861bc342cd5e3aabd7fe32be42b1c7
Colin Kligge <colinkligge156@gmail.com>
2025-10-03 18:48:33 -0400

    dark mode

---
+++ script.js
@@ -0,0 +1,33 @@
+let defaulted = document.getElementById("default");
+let darkMode = document.getElementById("darkmode")
+let lightMode = document.getElementById("lightmode");
+
+function defaultTheme() {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        console.log('Dark mode')
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        console.log('Light mode')
+    }
+}
+
+defaulted.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    document.body.style.backgroundColor = "";
+    document.body.style.color = "";
+});
+
+darkMode.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+});
+
+lightMode.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+});
